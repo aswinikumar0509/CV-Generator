3d9e54e77fc6a67bdbca7296ac4827a0d74bdfef
Aswini Kumar <aswini0509@gmail.com>
2024-07-11 07:00:42 +0530

    update mvt

--- cvgenerator/models.py
+++ cvgenerator/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+
+class Profile(models.Model):
+
+    name = models.CharField(max_length=100)
+    email = models.CharField(max_length=100)
+    phone = models.CharField(max_length=100)
+    summary = models.TextField(max_length=1000)
+    degree1 = models.CharField(max_length=100)
+    degree2 = models.CharField(max_length=100)
+    skills = models.TextField(max_length=1000)
+    projects = models.TextField(max_length=2000)
+    experience = models.TextField(max_length=2000)
+    language = models.CharField(max_length=100)
+
--- cvgenerator/views.py
+++ cvgenerator/views.py
@@ -1,3 +1,27 @@
 from django.shortcuts import render
 
+from . models import Profile
 # Create your views here.
+
+def accept(request):
+    if request.method=="POST":
+        name = request.POST.get("name" , "")
+        email = request.POST.get("email" , "")
+        phone = request.POST.get("phone" , "")
+        summary = request.POST.get("summary" , "")
+        degree1 = request.POST.get("degree1" , "")
+        degree2 = request.POST.get("degree2" , "")
+        skills = request.POST.get("skills" , "")
+        projects = request.POST.get("projects" , "")
+        experience = request.POST.get("experience" , "")
+        language = request.POST.get("language" , "")
+
+        profile = Profile(name=name,email=email,phone=phone,summary=summary,degree1=degree1,degree2=degree2,skills=skills,projects=projects,experience=experience,language=language)
+        profile.save()
+
+    return render(request , 'cvgenerator/accept.html')
+
+def resume(request,id):
+    user_profile = Profile.objects.get(id=id)
+
+    return render(request , 'cvgenerator/resume.html',{'user_profile': user_profile})
