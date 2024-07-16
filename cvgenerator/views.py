e4cf04e729e925cf1d4f16b37aafecbb31ad2442
Aswini Kumar <aswini0509@gmail.com>
2024-07-16 22:22:51 +0530

    views update

--- cvgenerator/views.py
+++ cvgenerator/views.py
@@ -2,9 +2,14 @@ from django.shortcuts import render
 
 from . models import Profile
 # Create your views here.
+import pdfkit
+from django.http import HttpResponse
+from django.template import loader
+import io
 
 def accept(request):
     if request.method=="POST":
+
         name = request.POST.get("name" , "")
         email = request.POST.get("email" , "")
         phone = request.POST.get("phone" , "")
@@ -22,6 +27,16 @@ def accept(request):
     return render(request , 'cvgenerator/accept.html')
 
 def resume(request,id):
-    user_profile = Profile.objects.get(id=id)
+    user_profiles = Profile.objects.get(pk=id)
 
-    return render(request , 'cvgenerator/resume.html',{'user_profile': user_profile})
+    template = loader.get_template('cvgenerator/resume.html')
+    html = template.render({'user_profile':user_profiles})
+    options ={
+        'page-size':'Letter',
+        'encoding':"UTF-8",
+    }
+    pdf = pdfkit.from_string(html,False,options)
+    response = HttpResponse(pdf,content_type='application/pdf')
+    response['Content-Disposition'] ='attachment'
+    filename = "resume.pdf"
+    return response
